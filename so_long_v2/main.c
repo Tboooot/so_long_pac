0be9c030589f976564b06fb8f1d64339b624c405
Mohamed Amine Tarza <mtarza@c2r6p2.1337.ma>
2025-02-11 08:00:39 +0100

    up

--- so_long_v2/main.c
+++ so_long_v2/main.c
@@ -1,55 +1,5 @@
 #include "includes/so_long.h"
 
-int close_game(t_game *game)
-{
-    if (game->mlx)
-    {
-        if (game->wall.img)
-            mlx_destroy_image(game->mlx, game->wall.img);
-        if (game->player.img)
-            mlx_destroy_image(game->mlx, game->player.img);
-        if (game->collectible.img)
-            mlx_destroy_image(game->mlx, game->collectible.img);
-        if (game->exit.img)
-            mlx_destroy_image(game->mlx, game->exit.img);
-        if (game->floor.img)
-            mlx_destroy_image(game->mlx, game->floor.img);
-        
-        if (game->win)
-            mlx_destroy_window(game->mlx, game->win);
-    }
-
-    if (game->map)
-    {
-        free_map(game->map);
-        free(game->map);
-    }
-
-    exit(0);
-    return (0);
-}
-
-int key_handler(int keycode, t_game *game)
-{
-    int x = game->map->player_x;
-    int y = game->map->player_y;
-
-    if (keycode == KEY_ESC)
-        return (close_game(game));
-    else if (keycode == KEY_W || keycode == KEY_UP)
-        y--;
-    else if (keycode == KEY_S || keycode == KEY_DOWN)
-        y++;
-    else if (keycode == KEY_A || keycode == KEY_LEFT)
-        x--;
-    else if (keycode == KEY_D || keycode == KEY_RIGHT)
-        x++;
-
-    if (x != game->map->player_x || y != game->map->player_y)
-        move_player(game, x, y);
-    return (0);
-}
-
 int main(int argc, char *argv[])
 {
     t_game  *game;
@@ -75,19 +25,5 @@ int main(int argc, char *argv[])
         return (1);
     }
 
-    if (!init_game(game))
-    {
-        free_map(game->map);
-        free(game);
-        return (1);
-    }
-
-    mlx_hook(game->win, X_EVENT_KEY_PRESS, 1L<<0, key_handler, game);
-    mlx_hook(game->win, X_EVENT_EXIT, 0, close_game, game);  // Handle window close button
-
-    render_game(game);
-
-    mlx_loop(game->mlx);
-
-    return (0);
+     return (0);
 }
