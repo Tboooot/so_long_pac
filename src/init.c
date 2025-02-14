8f945e716ea89f1c29d5dde31cdda6d93ee9bd40
Mohamed Amine Tarza <mtarza@c2r1p10.1337.ma>
2025-02-14 03:37:14 +0100

    up

--- src/init.c
+++ src/init.c
@@ -1,6 +1,46 @@
 #include "so_long.h"
 
 
+static void	set_game_objective(t_game *game)
+{
+	int	(i),(j);
+	
+	i = -1;
+	while (++i < game->map.n_row)
+	{
+		j = -1;
+		while (++j < game->map.n_col)
+			if (game->map.grid[i][j] == 'C')
+				game->collect++;
+	}
+}
+
+static void	*get_sprite(char *path, t_game *game)
+{
+	int		(width),(height);
+
+	width = game->size;
+	height = game->size;
+	return (mlx_xpm_file_to_image(game->id, path, &width, &height));
+}
+
+static void	init_sprites(t_game *game)
+{
+	if (game->size == 32)
+	{
+		game->img.collect =  get_sprite("assets/32/floor.xpm", game);
+		game->img.wall = get_sprite("assets/32/wall.xpm", game);
+		game->img.collect = get_sprite("assets/32/fruit.xpm", game);
+		game->img.r1 = get_sprite("assets/32/r1.xpm", game);
+		game->img.exit_open = get_sprite("assets/32/exit4.xpm", game);
+		return ;
+	}
+	game->img.collect = get_sprite("assets/64/floor.xpm", game);
+	game->img.wall = get_sprite("assets/64/wall.xpm", game);
+	game->img.collect = get_sprite("assets/64/fruit.xpm", game);
+	game->img.r1 = get_sprite("assets/64/r1.xpm", game);
+	game->img.exit_open = get_sprite("assets/64/exit4.xpm", game);
+}
 
 
 void init_window(t_game *game)
